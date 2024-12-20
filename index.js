ec4e665f438748842c17a4e20225f35dddb68db6
00berto <162624084+00berto@users.noreply.github.com>
2024-12-21 00:18:45 +0100

    upload file

---
+++ index.js
@@ -0,0 +1,156 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./jsx_css/App";
+
+const root = ReactDOM.createRoot(document.getElementById("carousel"));
+
+root.render(<App />);
+
+/*root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);*/
+
+// Función efecto maquina de escribir
+function typeWriter(htmlString, element, speed, callback) {
+  const parser = new DOMParser();
+  const parsedHTML = parser.parseFromString(htmlString, "text/html"); // Convertimos el HTML a nodos
+  const nodes = Array.from(parsedHTML.body.childNodes); // Obtenemos los nodos de la estructura HTML
+  let currentNodeIndex = 0;
+
+  function processNode() {
+    if (currentNodeIndex < nodes.length) {
+      const currentNode = nodes[currentNodeIndex];
+
+      if (currentNode.nodeType === Node.TEXT_NODE) {
+        // Animar un texto plano
+        const text = currentNode.textContent;
+        let textIndex = 0;
+
+        function typeText() {
+          if (textIndex < text.length) {
+            element.innerHTML += text[textIndex];
+            textIndex++;
+            setTimeout(typeText, speed);
+          } else {
+            currentNodeIndex++;
+            processNode();
+          }
+        }
+
+        typeText();
+      } else if (currentNode.nodeType === Node.ELEMENT_NODE) {
+        // Animar un elemento HTML
+        element.innerHTML += currentNode.outerHTML; // Añadimos directamente el elemento
+        currentNodeIndex++;
+        setTimeout(processNode, speed);
+      }
+    } else if (callback) {
+      callback();
+    }
+  }
+
+  processNode();
+}
+
+// Espera a que el DOM cargue
+document.addEventListener("DOMContentLoaded", () => {
+  const titulo = "HELLO WORLD \n I'M ALBERTO ZANI";
+  const formattedTitle = titulo.replaceAll("\n", "<br>");
+
+  const typewriterElement = document.getElementById("titulo");
+  const restContents = document.querySelectorAll(".rest-content");
+
+  // start animacion
+  typeWriter(formattedTitle, typewriterElement, 150, () => {
+    setTimeout(() => {
+      restContents.forEach((content) => {
+        content.style.display = "block";
+      });
+    }, 900);
+    // retraso aparicio en milisegundos (1000 = 1segundo)
+  });
+});
+
+/* Efecto box que si muovono */
+import * as basicScroll from "basicscroll";
+
+const boxes = document.querySelectorAll(".box");
+
+// Direcciones de movimiento para cada box
+const movimientos = [
+  { from: "-100px", to: "100px" }, // Box 1:  de izquierda a derecha
+  { from: "0px", to: "250px" }, //    Box 2:  de centro hacia la derecha
+  { from: "100px", to: "-200px" }, // Box 3:  de derecha a izquierda
+  { from: "50px", to: "-150px" }, //  Box 4:  de izquierda a derecha
+  { from: "-100px", to: "100px" }, // Box 5:  de izquierda a derecha
+  { from: "0px", to: "250px" }, //    Box 6:  de centro hacia la derecha
+  { from: "100px", to: "-200px" }, // Box 7:  de derecha a izquierda
+  { from: "50px", to: "-150px" }, //  Box 8:  de izquierda a derecha
+];
+
+function animateBoxes() {
+  requestAnimationFrame(animateBoxes);
+
+  boxes.forEach((box, index) => {
+    const movimiento = movimientos[index];
+
+    const instance = basicScroll.create({
+      elem: box,
+      from: "bottom-bottom",
+      to: "top-top",
+      direct: true,
+      props: {
+        "--r": {
+          from: "0",
+          to: "1turn",
+        },
+        "--tx": {
+          from: movimiento.from,
+          to: movimiento.to,
+        },
+      },
+    });
+
+    instance.start();
+  });
+}
+animateBoxes();
+
+/* aprir imagen */
+
+function createLightbox(selectors) {
+  selectors.forEach((selector) => {
+    document.querySelectorAll(selector).forEach((img) => {
+      img.addEventListener("click", () => {
+        const modal = document.createElement("div");
+        modal.style.cssText = `
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: rgba(0,0,0,0.8);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      z-index: 1000;
+    `;
+        const modalImg = document.createElement("img");
+        modalImg.src = img.getAttribute("src");
+        modalImg.style.maxWidth = "90%";
+        modalImg.style.maxHeight = "90%";
+        modal.appendChild(modalImg);
+        document.body.appendChild(modal);
+
+        modal.addEventListener("click", () => {
+          document.body.removeChild(modal);
+        });
+      });
+    });
+  });
+}
+
+// Uso
+createLightbox([".game-image img", ".fella img", ".porfolio img"]); // añadir, y entre "" poner .nomeClasseCSS img
---
+++ jsx_css/App.jsx
@@ -0,0 +1,78 @@
+/* REACT */
+
+/* Carousel icono */
+import React from "react";
+import Slider from "react-slick";
+import { cardDetails } from "./carousel/CarouselConfig";
+
+const IconCarousel = () => {
+  const settings = {
+    dots: false, //si false  no se veen los botones (en css:slick-prev o slick-before o slick-arrow)
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        maxWidth: "80%",
+        margin: "0 auto",
+        overflowX: "hidden",
+      }}
+    >
+      <Slider {...settings}>
+        {Object.keys(cardDetails).map((key) => {
+          const { imgSrc, title } = cardDetails[key];
+          return (
+            <div
+              key={key}
+              style={{
+                overflow: "hidden",
+                textAlign: "center",
+                width: "100%",
+                maxWidth: "100vw",
+              }}
+            >
+              <img
+                src={imgSrc}
+                alt={title}
+                className="imagen-icono"
+                style={{
+                  /*
+                  width: "80px",
+                  height: "80px",*/
+                  objectFit: "contain",
+                  margin: "auto",
+                }}
+                //style={{ width: 100, height: 100, marginBottom: 10 }}
+              />
+              <h4>{title}</h4>
+            </div>
+          );
+        })}
+      </Slider>
+    </div>
+  );
+};
+
+export default IconCarousel;
