aace302183493421a9732777b9c9e32e568515e2
berto <162624084+00berto@users.noreply.github.com>
2024-12-21 19:49:20 +0100

    restructura de las carpetas y codigo

---
+++ src/CarouselConfig.jsx
@@ -0,0 +1,32 @@
+import Icon1 from "./assets/image/html-1.svg";
+import Icon2 from "./assets/image/css-3.svg";
+import Icon3 from "./assets/image/javascript-1.svg";
+import Icon4 from "./assets/image/react-2.svg";
+import Icon5 from "./assets/image/python-5.svg";
+
+export const cardDetails = {
+  0: {
+    imgSrc: Icon5,
+    title: "Python",
+  },
+
+  1: {
+    imgSrc: Icon1,
+    title: "Html5",
+  },
+
+  2: {
+    imgSrc: Icon2,
+    title: "Css3",
+  },
+
+  3: {
+    imgSrc: Icon3,
+    title: "JavaScript",
+  },
+
+  4: {
+    imgSrc: Icon4,
+    title: "React",
+  },
+};
