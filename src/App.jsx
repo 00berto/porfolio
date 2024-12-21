12965ad45b17cfcdb6406a79581d1c5f23f5ee9b
berto <162624084+00berto@users.noreply.github.com>
2024-12-21 22:41:35 +0100

    migrar a vite

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@
 /* Carousel icono */
 import React from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { cardDetails } from "./CarouselConfig";
 
 const IconCarousel = () => {
