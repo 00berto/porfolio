9256783be938643fb6cc99912d1ef9322c1b9866
berto <162624084+00berto@users.noreply.github.com>
2025-02-01 01:37:30 +0100

    añadidos iconos

--- src/CarouselConfig.jsx
+++ src/CarouselConfig.jsx
@@ -2,11 +2,14 @@ import Icon1 from "./image/html-1.svg";
 import Icon2 from "./image/css-3.svg";
 import Icon3 from "./image/javascript-1.svg";
 import Icon4 from "./image/react-2.svg";
-import Icon5 from "./image/python-5.svg";
+import Icon0 from "./image/python-5.svg";
+import Icon6 from "./image/bootstrap-5-1.svg";
+import Icon7 from "./image/vercel.svg";
+//import Icon8 from "./image/python-5.svg";
 
 export const cardDetails = {
   0: {
-    imgSrc: Icon5,
+    imgSrc: Icon0,
     title: "Python",
   },
 
@@ -28,5 +31,15 @@ export const cardDetails = {
   4: {
     imgSrc: Icon4,
     title: "React",
+  },
+
+  5: {
+    imgSrc: Icon6,
+    title: "Bootstrap 5",
+  },
+
+  6: {
+    imgSrc: Icon7,
+    title: "Vercel",
   },
 };
